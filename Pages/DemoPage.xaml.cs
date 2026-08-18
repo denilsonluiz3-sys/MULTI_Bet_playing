7001092d6b1c8ea22dfb181837f763f6bfc6984d
denilsonluiz3-sys/AURA_assistente <denilsonluiz3@gmail.com>
2026-08-17 21:56:31 -0300

    Add DemoPage code-behind

---
+++ Pages/DemoPage.xaml.cs
@@ -0,0 +1,58 @@
+using MULTI_Bet_playing_Demo.Services;
+
+namespace MULTI_Bet_playing_Demo.Pages;
+
+public partial class DemoPage : ContentPage
+{
+    private readonly CardService _cardService;
+    private readonly WebView[] _webViews;
+    private readonly Label[] _labels;
+
+    public DemoPage() : this(ResolveCardService()) { }
+
+    public DemoPage(CardService cardService)
+    {
+        InitializeComponent();
+        _cardService = cardService;
+        _webViews = new[] { WebView1, WebView2, WebView3, WebView4 };
+        _labels = new[] { Label1, Label2, Label3, Label4 };
+    }
+
+    private static CardService ResolveCardService() =>
+        Application.Current?.Handler?.MauiContext?.Services?.GetService<CardService>()
+        ?? new CardService();
+
+    private async void OnLoadFavorites(object? sender, EventArgs e)
+    {
+        var favorites = await _cardService.GetFavoritesAsync();
+        if (favorites.Count == 0)
+        {
+            await DisplayAlert("Aviso", "Nenhum favorito encontrado. Adicione cassinos na aba Início.", "OK");
+            return;
+        }
+
+        for (int i = 0; i < _webViews.Length; i++)
+        {
+            if (i < favorites.Count)
+            {
+                _webViews[i].Source = favorites[i].Url;
+                _labels[i].Text = favorites[i].Title;
+                await _cardService.MarkUsedAsync(favorites[i].Id);
+            }
+            else
+            {
+                _webViews[i].Source = null;
+                _labels[i].Text = $"Tela {i + 1}";
+            }
+        }
+    }
+
+    private void OnClearAll(object? sender, EventArgs e)
+    {
+        for (int i = 0; i < _webViews.Length; i++)
+        {
+            _webViews[i].Source = null;
+            _labels[i].Text = $"Tela {i + 1}";
+        }
+    }
+}
